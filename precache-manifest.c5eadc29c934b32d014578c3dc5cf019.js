b5d20e9945691c3ea394d0a707bc6044ac705eab
nmjd <spectrermn@gmail.com>
2020-10-24 01:46:20 +0200

    Updates

---
+++ precache-manifest.c5eadc29c934b32d014578c3dc5cf019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf371f562679f4be86a67bd3e6d851dc",
+    "url": "/CovidNewsApp/index.html"
+  },
+  {
+    "revision": "c883e5cfa0d5729020e9",
+    "url": "/CovidNewsApp/static/css/main.1cf4861e.chunk.css"
+  },
+  {
+    "revision": "6b34ab712e59a45e70c4",
+    "url": "/CovidNewsApp/static/js/2.0dd5154d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CovidNewsApp/static/js/2.0dd5154d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c883e5cfa0d5729020e9",
+    "url": "/CovidNewsApp/static/js/main.d4d1541d.chunk.js"
+  },
+  {
+    "revision": "ca6e69ddfe27c9984525",
+    "url": "/CovidNewsApp/static/js/runtime-main.3d7d3398.js"
+  }
+]);
